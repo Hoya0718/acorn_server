563111a3a8254e4a9f1c61043e5cc117a07f984c
SeungHee <mnb2856@gmail.com>
2024-06-25 11:41:17 +0900

    Merge branch 'master' into seunghee

--- src/main/java/com/acorn/erp/domain/Customer/Repository/RegionGroupRepository.java
+++ src/main/java/com/acorn/erp/domain/Customer/Repository/RegionGroupRepository.java
@@ -3,5 +3,5 @@ package com.acorn.erp.domain.Customer.Repository;
 import com.acorn.erp.domain.Customer.Entity.RegionGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RegionGroupRepository extends JpaRepository<RegionGroup, String> {
+public interface RegionGroupRepository extends JpaRepository<RegionGroup, Integer> {
 }
