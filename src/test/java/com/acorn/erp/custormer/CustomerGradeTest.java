d3d6f6dc535a7ed8ba0ea0a2393bdd66844176bc
SeungHee <mnb2856@gmail.com>
2024-07-01 09:36:12 +0900

    회원등급 테스트 수정

--- src/test/java/com/acorn/erp/custormer/CustomerGradeTest.java
+++ src/test/java/com/acorn/erp/custormer/CustomerGradeTest.java
@@ -4,24 +4,23 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import com.acorn.erp.domain.Customer.Service.PreferenceService;
+import com.acorn.erp.domain.Customer.Service.GradeService;
 
 
 @SpringBootTest
 public class CustomerGradeTest implements CommandLineRunner {
 
     @Autowired
-    private PreferenceService service;
+    private  GradeService service;
 
         @Override
         public void run(String... args) throws Exception {
-        	System.out.println("Running AgeGroupTests...");
+        	System.out.println("Running Customer Grade Tests...");
             test();
-        	
+   
         };
         @Test
         public void test() {
-            service.calculateOrderData();
+            service.applyDefaultGradeToExistingCustomers();
         }
 }
