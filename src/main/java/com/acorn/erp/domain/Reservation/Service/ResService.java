3aa40958c4e1bef7d5caad8fcf4ef3ae3f6e4ed7
sireal <140906501+sijin1017@users.noreply.github.com>
2024-07-02 15:53:30 +0900

    Update ResService.java

--- src/main/java/com/acorn/erp/domain/Reservation/Service/ResService.java
+++ src/main/java/com/acorn/erp/domain/Reservation/Service/ResService.java
@@ -1,37 +1,47 @@
 package com.acorn.erp.domain.Reservation.Service;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.acorn.erp.domain.Reservation.Entity.Reservation;
-import com.acorn.erp.domain.Reservation.Repository.ResMapper;
+import com.acorn.erp.domain.Reservation.Repository.ResRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class ResService {
 
     @Autowired
-    private ResMapper resMapper;
+    private ResRepository resRepository;
 
+    @Transactional
     public List<Reservation> getAllReservations() {
-        return resMapper.getAllReservations();
+        return resRepository.findAll();
     }
-
-    public Reservation getReservation(int id) {
-        return resMapper.getReservation(id);
+    @Transactional
+    public Reservation getReservation(Long id) {
+        return resRepository.findById(id).orElse(null);
     }
-
-    public void insertReservation(Reservation reservation) {
-        resMapper.insertReservation(reservation);
+    @Transactional
+    public Reservation insertReservation(Reservation reservation) {
+    	System.out.println("Inserting reservation: " + reservation);
+        return resRepository.save(reservation);
     }
-
-    public void deleteReservation(int id) {
-        resMapper.deleteReservation(id);
+    @Transactional
+    public void deleteReservation(Long id) {
+        resRepository.deleteById(id);
     }
-
+    @Transactional
     public void updateReservation(Reservation reservation) {
-        resMapper.updateReservation(reservation);
+        resRepository.save(reservation);
         System.out.println("서비스 성공");
+    }
+    
+    @Transactional
+    public Reservation saveReservation(Reservation reservation) {
+        // 로그 추가
+        System.out.println("Saving reservation: " + reservation);
+        return resRepository.save(reservation);
     }
 }
