44d9e94f94cd43d4687d98c699ca231734f305c1
zkszka <tenandten10@naver.com>
2024-07-04 14:44:11 +0900

    7/4 발주, 거래처, 물류 연동 성공

--- src/main/java/com/acorn/erp/domain/Customer/Repository/AgeGroupRepository.java
+++ src/main/java/com/acorn/erp/domain/Customer/Repository/AgeGroupRepository.java
@@ -1,15 +1,15 @@
-//package com.acorn.erp.domain.Customer.Repository;
-//
-//import java.util.List;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import com.acorn.erp.domain.Customer.Entity.AgeGroup;
-//import com.acorn.erp.domain.Customer.Entity.CustomerInfo;
-//
-//public interface AgeGroupRepository extends JpaRepository<AgeGroup, Integer>{
-//	// customerId를 기준으로 존재 여부 확인
-//    boolean existsByCustomerId(int customerId);
-//    
-//    List<AgeGroup> findByCustomerId(int customerId);
-//}
+package com.acorn.erp.domain.Customer.Repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.acorn.erp.domain.Customer.Entity.AgeGroup;
+import com.acorn.erp.domain.Customer.Entity.CustomerInfo;
+
+public interface AgeGroupRepository extends JpaRepository<AgeGroup, Integer>{
+	// customerId를 기준으로 존재 여부 확인
+    boolean existsByCustomerId(int customerId);
+    
+    List<AgeGroup> findByCustomerId(int customerId);
+}
