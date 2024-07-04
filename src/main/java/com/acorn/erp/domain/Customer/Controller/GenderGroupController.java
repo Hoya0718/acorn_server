3c947132ead86901f8d5ded3ed57fd32991257ef
zkszka <tenandten10@naver.com>
2024-07-04 14:44:11 +0900

    7/4 발주, 거래처, 물류 연동 성공

--- src/main/java/com/acorn/erp/domain/Customer/Controller/GenderGroupController.java
+++ src/main/java/com/acorn/erp/domain/Customer/Controller/GenderGroupController.java
@@ -1,25 +1,25 @@
-//package com.acorn.erp.domain.Customer.Controller;
-//
-//
-//import java.util.Map;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import com.acorn.erp.domain.Customer.Repository.AgeGroupRepository;
-//import com.acorn.erp.domain.Customer.Service.GenderGroupService;
-//
-//@RestController
-//@RequestMapping("/api/customer")
-//public class GenderGroupController {
-//	
-//    @Autowired
-//    private GenderGroupService service;
-//
-//    @GetMapping("/count_gender_group")
-//    public Map<String, Long> countGenderGroup(){
-//        return service.countCustomersByGenderGroup();
-//    }
-//}
+package com.acorn.erp.domain.Customer.Controller;
+
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.acorn.erp.domain.Customer.Repository.AgeGroupRepository;
+import com.acorn.erp.domain.Customer.Service.GenderGroupService;
+
+@RestController
+@RequestMapping("/api/customer")
+public class GenderGroupController {
+	
+    @Autowired
+    private GenderGroupService service;
+
+    @GetMapping("/count_gender_group")
+    public Map<String, Long> countGenderGroup(){
+        return service.countCustomersByGenderGroup();
+    }
+}
--- src/main/java/com/acorn/erp/domain/Customer/Repository/CustomerInfoRepository.java
+++ src/main/java/com/acorn/erp/domain/Customer/Repository/CustomerInfoRepository.java
@@ -1,18 +1,18 @@
-//package com.acorn.erp.domain.Customer.Repository;
-//
-//import java.util.Date;
-//import java.util.List;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.jpa.repository.Query;
-//import org.springframework.data.repository.query.Param;
-//
-//import com.acorn.erp.domain.Customer.Entity.AgeGroup;
-//import com.acorn.erp.domain.Customer.Entity.CustomerInfo;
-//
-//public interface CustomerInfoRepository extends JpaRepository<CustomerInfo, Integer>{
-//	@Query("SELECT COUNT(c) FROM CustomerInfo c WHERE c.registerDate <= :endDate")
-//    long countCustomersLastyear(@Param("endDate") Date endDate);
-//    
-//	List<CustomerInfo> findByCustomerId(int customerId);
-//}
+package com.acorn.erp.domain.Customer.Repository;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.acorn.erp.domain.Customer.Entity.AgeGroup;
+import com.acorn.erp.domain.Customer.Entity.CustomerInfo;
+
+public interface CustomerInfoRepository extends JpaRepository<CustomerInfo, Integer>{
+	@Query("SELECT COUNT(c) FROM CustomerInfo c WHERE c.registerDate <= :endDate")
+    long countCustomersLastyear(@Param("endDate") Date endDate);
+    
+	List<CustomerInfo> findByCustomerId(int customerId);
+}
