e6ad515353bc2ecf26c266f623d8042523b59812
TakHoya <130663193+Hoya0718@users.noreply.github.com>
2024-07-05 18:39:53 +0900

    commit 2027/07/05 18:40

--- src/main/java/com/acorn/erp/domain/Sales/Service/OrderService.java
+++ src/main/java/com/acorn/erp/domain/Sales/Service/OrderService.java
@@ -15,3 +15,27 @@
 //        this.orderRepository = orderRepository;
 //    }
 //}
+
+package com.acorn.erp.domain.Sales.Service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.acorn.erp.domain.Sales.Entity.OrderTable;
+import com.acorn.erp.domain.Sales.Repository.OrderRepository;
+
+@Service
+public class OrderService {
+private final OrderRepository orderRepository;
+
+    @Autowired
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+    
+    public List<OrderTable> getAllItems() {
+        return orderRepository.findAll();
+    }
+}
