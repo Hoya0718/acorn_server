62a8181bb65f139398d06717e7e23e656ee3bc50
TakHoya <130663193+Hoya0718@users.noreply.github.com>
2024-07-05 11:16:25 +0900

    로그인 기능 구현 세션 포함 2024-07-05 11:16

--- src/main/java/com/acorn/erp/domain/Login/Repository/UserInfoMapper.java
+++ src/main/java/com/acorn/erp/domain/Login/Repository/UserInfoMapper.java
@@ -1,21 +1,32 @@
 package com.acorn.erp.domain.Login.Repository;
 
+
+
 import java.util.List;
 
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
-import org.springframework.stereotype.Repository;
+
 
 import com.acorn.erp.domain.Login.Entity.*;
 
 @Mapper
 public interface UserInfoMapper {
-	@Select("select * from userinfo")
-	List<userInfo> getAllUserInfo();
 	
+	 @Select("SELECT * FROM USERINFO")
+	    List<userInfo> getAllUserInfo();
+	
+	 @Select("SELECT u FROM userinfo WHERE registrationNum = #{registrationNum} AND email = #{email}")
+	 String getFindPw(@Param("registrationNum") String registrationNum, @Param("email") String email);
+	
+	 @Select("SELECT * FROM userInfo where email = #{email}")
+	 userInfo getUserByEmail(String email);
+	 	
+	 
 	@Insert("insert into userInfo(email, password, shopName, registrationNum) values(#{email}, #{password}, #{shopName}, #{registrationNum})")
 	void insertUserInfo(userInfo userinfo); 
 	
@@ -25,6 +36,9 @@ public interface UserInfoMapper {
 	@Update("UPDATE userInfo SET password = #{password} WHERE email =#{email}")
 	void updateUserInfo(userInfo userinfo);
 	
+	//로그인 구현
+	@Select("SELECT * FROM USERINFO WHERE email = #{email} AND password = #{password}")
+    userInfo getUserByEmailAndPassword(@Param("email") String email, @Param("password") String password);
 	
 }
 	
