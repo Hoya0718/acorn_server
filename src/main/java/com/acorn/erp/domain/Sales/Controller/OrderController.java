d66f8f3781e68829031ca91e15432b6d23a8873b
zkszka <tenandten10@naver.com>
2024-07-05 17:59:55 +0900

    7/5 재무, 커뮤니티 생성 첫번째

--- src/main/java/com/acorn/erp/domain/Sales/Controller/OrderController.java
+++ src/main/java/com/acorn/erp/domain/Sales/Controller/OrderController.java
@@ -1,18 +1,27 @@
-//package com.acorn.erp.domain.Sales.Controller;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import com.acorn.erp.domain.Sales.Service.OrderService;
-//
-//@RestController
-//@RequestMapping("/api/orders")
-//public class OrderController {
-//    private final OrderService orderService;
-//
-//    @Autowired
-//    public OrderController(OrderService orderService) {
-//        this.orderService = orderService;
-//    }
-//}
+package com.acorn.erp.domain.Sales.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.acorn.erp.domain.Sales.Entity.OrderTable;
+import com.acorn.erp.domain.Sales.Service.OrderService;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+    private final OrderService orderService;
+
+    @Autowired
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+    
+    @GetMapping
+    public List<OrderTable> getAllItems() {
+        return orderService.getAllItems();
+    }
+}
--- src/main/java/com/acorn/erp/domain/Sales/Entity/Item.java
+++ src/main/java/com/acorn/erp/domain/Sales/Entity/Item.java
@@ -5,12 +5,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@Table(name = "SALES_ITEM")
 public class Item {
     @Id //primary key
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
