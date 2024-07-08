0f6b848a7a259fc854f0e584cb492f8b6c7949c2
SeungHee <mnb2856@gmail.com>
2024-07-08 12:12:02 +0900

    특이사항 수정 삭제 기능

--- src/main/java/com/acorn/erp/domain/Customer/Controller/NotesController.java
+++ src/main/java/com/acorn/erp/domain/Customer/Controller/NotesController.java
@@ -10,10 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.acorn.erp.domain.Customer.Entity.CustomerNotes;
@@ -45,8 +45,9 @@ public class NotesController {
     	 CustomerNotes savedNote = repository.save(customerNotes);
          return ResponseEntity.ok(savedNote);
     }
-    @DeleteMapping("/notes")
-    public ResponseEntity<?> deleteNotes(@RequestParam("notesId")  Integer notesId){
+    @DeleteMapping("/notes/{notesId}")
+    public ResponseEntity<?> deleteNotes(@PathVariable("notesId")  Integer notesId){
+    	System.out.println("Received notesId: " + notesId);
     	if (notesId == null) {
             return ResponseEntity.badRequest().body("Notes ID cannot be null");
         }
