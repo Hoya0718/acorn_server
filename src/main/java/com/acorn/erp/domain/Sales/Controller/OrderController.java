8035d434d382449b9b6c6aa0333f8af8a7200951
yewon5 <ik_324@naver.com>
2024-07-09 18:32:17 +0900

    sales

--- src/main/java/com/acorn/erp/domain/Sales/Controller/OrderController.java
+++ src/main/java/com/acorn/erp/domain/Sales/Controller/OrderController.java
@@ -3,7 +3,15 @@ package com.acorn.erp.domain.Sales.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.acorn.erp.domain.Sales.Entity.OrderTable;
 import com.acorn.erp.domain.Sales.Service.OrderService;
@@ -32,5 +40,24 @@ public class OrderController {
     @PostMapping
     public OrderTable addOrder(@RequestBody OrderTable order) {
         return orderService.addOrder(order);
+    }
+
+    @PutMapping("/{orderNum}")
+    public ResponseEntity<OrderTable> updateOrder(@PathVariable("orderNum") Long orderNum, @RequestBody OrderTable order) {
+        OrderTable updatedOrder = orderService.updateOrder(orderNum, order);
+        if (updatedOrder != null) {
+            return ResponseEntity.ok(updatedOrder);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{orderNum}")
+    public ResponseEntity<Void> deleteOrder(@PathVariable("orderNum") Long orderNum) {
+        if (orderService.deleteOrder(orderNum)) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
